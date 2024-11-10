b3a2d32f2b80558dd6dd568ad209432abd56e74e
mihaituta <tutamihai@gmail.com>
2024-11-10 15:55:55 +0200

    initial commit

---
+++ src/main/java/com/tm/controller/Restaurant.java
@@ -0,0 +1,23 @@
+package com.tm.controller;
+import jakarta.persistence.*;
+
+@Entity
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String slug;
+    private Integer user_id;
+    private String city;
+    private String aboutText;
+    private String previewDescription;
+    private String previewImage;
+    private String logoImage;
+    private String contactText;
+    private String phone1;
+    private String phone2;
+    private String mail1;
+    private String mail2;
+}
