54f2fefcb8995ace91793f49356b38a0a60280da
mihaituta <tutamihai@gmail.com>
2024-11-16 22:24:09 +0200

    removed @Repository from RestaurantRepository, re-built jar file

--- src/main/java/com/tm/repository/RestaurantRepository.java
+++ src/main/java/com/tm/repository/RestaurantRepository.java
@@ -2,12 +2,10 @@ package com.tm.repository;
 
 import com.tm.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     Optional<Restaurant> findBySlug(String slug);
     Optional<Restaurant> findByUserId(Long userId);
