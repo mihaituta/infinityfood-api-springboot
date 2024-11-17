a472924b79e52271375d58117683f9524658c767
mihaituta <tutamihai@gmail.com>
2024-11-17 18:58:54 +0200

    added PATCH to CORS options

--- src/main/java/com/tm/config/SecurityConfig.java
+++ src/main/java/com/tm/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://infinityfood.netlify.app"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
 
